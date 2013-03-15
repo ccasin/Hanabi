50a9d3a530b6ce0c091a5d1f0a038242f93ec05a
Chris Casinghino <ccasin@cis.upenn.edu>
2013-03-15 17:14:21 -0400

    smaller cards, hooray

--- Settings/StaticFiles.hs
+++ Settings/StaticFiles.hs
@@ -30,44 +30,44 @@ smallColorImage c =
 cardToRoute :: Card -> StaticRoute
 cardToRoute c =
     case c of
-      Card Red One      -> img_red1_png
-      Card Red Two      -> img_red2_png      
-      Card Red Three    -> img_red3_png
-      Card Red Four     -> img_red4_png
-      Card Red Five     -> img_red5_png
-      Card Blue One     -> img_blue1_png
-      Card Blue Two     -> img_blue2_png      
-      Card Blue Three   -> img_blue3_png
-      Card Blue Four    -> img_blue4_png
-      Card Blue Five    -> img_blue5_png
-      Card Green One    -> img_green1_png
-      Card Green Two    -> img_green2_png      
-      Card Green Three  -> img_green3_png
-      Card Green Four   -> img_green4_png
-      Card Green Five   -> img_green5_png
-      Card Yellow One   -> img_yellow1_png
-      Card Yellow Two   -> img_yellow2_png      
-      Card Yellow Three -> img_yellow3_png
-      Card Yellow Four  -> img_yellow4_png
-      Card Yellow Five  -> img_yellow5_png
-      Card Pink One     -> img_pink1_png
-      Card Pink Two     -> img_pink2_png      
-      Card Pink Three   -> img_pink3_png
-      Card Pink Four    -> img_pink4_png
-      Card Pink Five    -> img_pink5_png
+      Card Red One      -> img_red1_small_png
+      Card Red Two      -> img_red2_small_png      
+      Card Red Three    -> img_red3_small_png
+      Card Red Four     -> img_red4_small_png
+      Card Red Five     -> img_red5_small_png
+      Card Blue One     -> img_blue1_small_png
+      Card Blue Two     -> img_blue2_small_png      
+      Card Blue Three   -> img_blue3_small_png
+      Card Blue Four    -> img_blue4_small_png
+      Card Blue Five    -> img_blue5_small_png
+      Card Green One    -> img_green1_small_png
+      Card Green Two    -> img_green2_small_png      
+      Card Green Three  -> img_green3_small_png
+      Card Green Four   -> img_green4_small_png
+      Card Green Five   -> img_green5_small_png
+      Card Yellow One   -> img_yellow1_small_png
+      Card Yellow Two   -> img_yellow2_small_png      
+      Card Yellow Three -> img_yellow3_small_png
+      Card Yellow Four  -> img_yellow4_small_png
+      Card Yellow Five  -> img_yellow5_small_png
+      Card Pink One     -> img_pink1_small_png
+      Card Pink Two     -> img_pink2_small_png      
+      Card Pink Three   -> img_pink3_small_png
+      Card Pink Four    -> img_pink4_small_png
+      Card Pink Five    -> img_pink5_small_png
 
 knowledgeToRoute :: Knowledge -> StaticRoute
 knowledgeToRoute k =
   case k of
     Knowledge (Is c)      (Is r) -> cardToRoute (Card c r)
-    Knowledge (Is Red)    _      -> img_redmystery_png
-    Knowledge (Is Blue)   _      -> img_bluemystery_png
-    Knowledge (Is Green)  _      -> img_greenmystery_png
-    Knowledge (Is Yellow) _      -> img_yellowmystery_png
-    Knowledge (Is Pink)   _      -> img_pinkmystery_png
-    Knowledge _       (Is One)   -> img_mystery1_png
-    Knowledge _       (Is Two)   -> img_mystery2_png
-    Knowledge _       (Is Three) -> img_mystery3_png
-    Knowledge _       (Is Four)  -> img_mystery4_png
-    Knowledge _       (Is Five)  -> img_mystery5_png
-    Knowledge _           _      -> img_mystery_png       
+    Knowledge (Is Red)    _      -> img_redmystery_small_png
+    Knowledge (Is Blue)   _      -> img_bluemystery_small_png
+    Knowledge (Is Green)  _      -> img_greenmystery_small_png
+    Knowledge (Is Yellow) _      -> img_yellowmystery_small_png
+    Knowledge (Is Pink)   _      -> img_pinkmystery_small_png
+    Knowledge _       (Is One)   -> img_blank1_small_png
+    Knowledge _       (Is Two)   -> img_blank2_small_png
+    Knowledge _       (Is Three) -> img_blank3_small_png
+    Knowledge _       (Is Four)  -> img_blank4_small_png
+    Knowledge _       (Is Five)  -> img_blank5_small_png
+    Knowledge _           _      -> img_mystery_small_png       
